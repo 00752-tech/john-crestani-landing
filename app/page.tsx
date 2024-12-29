65d30b617e5b400088facd4cdb8058cc5bfbd4dc
00752-tech <passiveincomestreaming@gmail.com>
2024-12-29 15:05:01 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,32 @@
-import Header from './components/Header'
-import Hero from './components/Hero'
-import Proof from './components/Proof'
-import Background from './components/Background'
-import AffiliateMarketing from './components/AffiliateMarketing'
-import Courses from './components/Courses'
-import SuccessStories from './components/SuccessStories'
-import FAQ from './components/FAQ'
-import CTA from './components/CTA'
-import Footer from './components/Footer'
-import CourseEvolution from './components/CourseEvolution'
-import FomoBanner from './components/FomoBanner'
+import { Header } from './components/Header'
+import { Hero } from './components/Hero'
+import { Proof } from './components/Proof'
+import { Background } from './components/Background'
+import { AffiliateMarketing } from './components/AffiliateMarketing'
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-black">
-      <FomoBanner />
+    <div className="flex flex-col min-h-screen">
       <Header />
-      <main>
+      <main className="flex-1">
         <Hero />
-        <CourseEvolution />
-        <Proof />
         <Background />
+        <Proof />
         <AffiliateMarketing />
-        <Courses />
-        <SuccessStories />
-        <FAQ />
-        <CTA />
       </main>
-      <Footer />
+      <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
+        <p className="text-xs text-gray-500 dark:text-gray-400">
+          © 2025 JohnCrestani.me. All rights reserved.
+        </p>
+        <nav className="sm:ml-auto flex gap-4 sm:gap-6">
+          <a href="#" className="text-xs hover:underline underline-offset-4">
+            Terms of Service
+          </a>
+          <a href="#" className="text-xs hover:underline underline-offset-4">
+            Privacy
+          </a>
+        </nav>
+      </footer>
     </div>
   )
 }
