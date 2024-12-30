8b755d3ce9beb36eca05a98ae5251a314c1cf3cf
00752-tech <passiveincomestreaming@gmail.com>
2024-12-30 10:24:13 -0800

    Update next.config.js
    Update next.config.js for Tailwind CSS and MDX support

--- next.config.js
+++ next.config.js
@@ -2,8 +2,16 @@ const withMDX = require('@next/mdx')()
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   reactStrictMode: true,
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
+    return config
+  },
 }
 
 module.exports = withMDX(nextConfig)
+
